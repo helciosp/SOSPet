70f2be5089ffd732fbef4bb4641ab8707fdbf597
helciosp43 <helciosp43@gmail.com>
2021-07-29 08:31:53 -0300

    Limpando

--- wwwroot/js/SOSPet.js
+++ wwwroot/js/SOSPet.js
@@ -2,7 +2,6 @@
 
 const menuSection = document.querySelector("#header")
 const menuToggle = menuSection.querySelector(".hamburger")
-const link = menuSection.querySelector("a")
 
 menuToggle.addEventListener("click", () => {
 
@@ -10,9 +9,4 @@ menuToggle.addEventListener("click", () => {
 
     menuSection.classList.toggle("on", show)
     show = !show;
-})
-
-link.addEventListener("click", () => {
-
-    menuSection.classList.toggle("off")
 })
