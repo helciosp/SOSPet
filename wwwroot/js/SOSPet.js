d2322482743d8507268f972ebb3945bc3fd1bf95
helciosp43 <helciosp43@gmail.com>
2021-07-30 14:11:56 -0300

    Fazendo rodapé ficar sempre no final da página

--- wwwroot/js/SOSPet.js
+++ wwwroot/js/SOSPet.js
@@ -1,7 +1,9 @@
-﻿let show = true; 
+﻿let show = true;
 
 const menuSection = document.querySelector("#header")
 const menuToggle = menuSection.querySelector(".hamburger")
+
+const footer = document.querySelector("footer")
 
 menuToggle.addEventListener("click", () => {
 
@@ -10,3 +12,22 @@ menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     show = !show;
 })
+
+$(document).ready(function () {
+    let alturaViewport = $(window).height();
+    let alturaElemento = $("main").height();
+    let distanciaTopo = $("main").offset().top;
+    let diferenca = alturaElemento - alturaViewport;
+
+    $(window).scroll(function () {
+
+        let posicaoScroll = $(window).scrollTop();
+
+        if (posicaoScroll < (distanciaTopo + diferenca)) {
+            footer.style.position = "fixed"
+        }
+        else {
+            footer.style.position = "initial"
+        }
+    });
+});
